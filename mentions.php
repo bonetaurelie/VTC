7064f10f9bf5ddd31ee9a2872b3b3372ce715452
bonetaurelie <bonetaurelie@yahoo.fr>
2021-07-14 16:30:54 +0200

    creation de mentions.php

---
+++ mentions.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="keywords" content="boutique chaussures paris, magasin chaussures de qualité, chaussures de qualité, chaussures en cuir, chaussures cousu Goodyear, bon rapport qualité prix, chaussures de luxe, chaussures luxe hommes, souliers, souliers hommes, bottes et bottines hommes, chaussures derby hommes, mocassin hommes, chaussures richelieu homme, chaussures daim hommes">
+    <meta name="description" content="Markowski chausseur boutique en ligne de chaussures haut de gamme de qualité pour hommes. Fabrication et vente en ligne de chaussure haut de gamme cousu Goodyear, bon rapport qualité prix, richelieu hommes, derby hommes, mocassin hommes, boots et bottines hommes et femmes, accessoires, chaussures de ville, chaussures de bureau, chaussures casual.">
+    <link rel="apple-touch-icon" sizes="57x57" href="img/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="Mimg/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="img/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="img/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="img/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="img/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="img/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="img/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="img/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192"  href="img/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="img/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="img/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="img/favicon-16x16.png">
+    <link rel="manifest" href="/manifest.json">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="font-awesome-4.7.0/css/font-awesome.min.css">
+
+</head>
+<body itemscope itemtype="http://schema.org/WebPage">
+    <div class="container corps">
+
+        <!--HEADER-->
+        <div itemscope itemtype="http://schema.org/WPHeader" class="row">
+            <img itemprop="description" src="img/markowski3.png" alt="markowski" title="markowski" class="markowski">
+        </div>
+
+        <!-- <div class="row">
+            <img itemprop="description" src="img/rea.png" alt="réalisateur de chaussures de qualité" title="réalisateur de chaussures de qualité" class="rea">
+        </div> -->
+
+        <!--MENTIONS LEGALES-->
+        <h1 itemprop="name">Mentions légales</h1>
+        
+        <div itemscope itemtype="http://schema.org/Thing" class="mentionslegales">
+            <p itemprop="description"><span><b>Editeur du Site : </b></span><br>
+                SAS AREGE<br>
+                Numéro de SIRET :  75311915500012<br>
+                Responsable editorial : Monsieur Georges VITAL<br>
+                46 rue Paradis 75010 PARIS<br>
+                Téléphone :+ 33(0)1 48 00 83 07<br>
+                Email : contact@prince-jorge.com<br>
+                Site Web : <a href="http://http://www.markowski.fr">http://www.markowski.fr</a>
+            </p>
+            <p itemprop="description"><b><span>Hébergement :</span></b><br>
+                Hébergeur : OVH<br>
+                2 rue Kellermann – 59100 Roubaix<br>
+                Site Web :  <a href="http://https://www.ovh.com/fr/">https://www.ovh.com/fr/</a>
+            </p>
+            <p itemprop="description"<span><b>Développement</b></span> : <br>
+                Aurélie BONET<br>
+                Adresse : 10 place Revoul 8460 VALREAS<br>
+                Site Web : <a href="http://http://aureliebonet.fr/">http://aureliebonet.fr/</a>
+            </p>
+        </div>
+    </div>
+
+
+    <!--FOOTER-->
+    <footer>
+        <div itemscope itemtype="http://schema.org/WPFooter" class="container-fluid footer">
+            <div><p itemprop="copyrightHolder" class="copyright">Copyright <i class="fa fa-copyright" aria-hidden="true"></i> Markowski 2017</p></div>
+            <div><a itemprop="url" class="mentions" href="mentions.php" target="_blank">Mentions légales</a></div>
+            <div><p itemprop="description" class="reseaux">Suivez-nous sur <a href="https://www.facebook.com/princejorgeparis/" target="_blank"><i class="fa fa-facebook" aria-hidden="true"></i></a> et <a href="#" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i></i></a></p></div>
+        </div>
+    </footer>
+
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+</body>
+</html>
