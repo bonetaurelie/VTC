2dc59dff2874528bef525282e64e90a5ffe647fe
bonetaurelie <bonetaurelie@yahoo.fr>
2021-07-15 11:26:59 +0200

    mise à jour contenu de la page

--- mentions.php
+++ mentions.php
@@ -4,26 +4,12 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="keywords" content="boutique chaussures paris, magasin chaussures de qualité, chaussures de qualité, chaussures en cuir, chaussures cousu Goodyear, bon rapport qualité prix, chaussures de luxe, chaussures luxe hommes, souliers, souliers hommes, bottes et bottines hommes, chaussures derby hommes, mocassin hommes, chaussures richelieu homme, chaussures daim hommes">
-    <meta name="description" content="Markowski chausseur boutique en ligne de chaussures haut de gamme de qualité pour hommes. Fabrication et vente en ligne de chaussure haut de gamme cousu Goodyear, bon rapport qualité prix, richelieu hommes, derby hommes, mocassin hommes, boots et bottines hommes et femmes, accessoires, chaussures de ville, chaussures de bureau, chaussures casual.">
-    <link rel="apple-touch-icon" sizes="57x57" href="img/apple-icon-57x57.png">
-    <link rel="apple-touch-icon" sizes="60x60" href="Mimg/apple-icon-60x60.png">
-    <link rel="apple-touch-icon" sizes="72x72" href="img/apple-icon-72x72.png">
-    <link rel="apple-touch-icon" sizes="76x76" href="img/apple-icon-76x76.png">
-    <link rel="apple-touch-icon" sizes="114x114" href="img/apple-icon-114x114.png">
-    <link rel="apple-touch-icon" sizes="120x120" href="img/apple-icon-120x120.png">
-    <link rel="apple-touch-icon" sizes="144x144" href="img/apple-icon-144x144.png">
-    <link rel="apple-touch-icon" sizes="152x152" href="img/apple-icon-152x152.png">
-    <link rel="apple-touch-icon" sizes="180x180" href="img/apple-icon-180x180.png">
-    <link rel="icon" type="image/png" sizes="192x192"  href="img/android-icon-192x192.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="img/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="96x96" href="img/favicon-96x96.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="img/favicon-16x16.png">
-    <link rel="manifest" href="/manifest.json">
+    <meta name="keywords" content="">
+    <meta name="description" content="">
+    
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="font-awesome-4.7.0/css/font-awesome.min.css">
 
 </head>
 <body itemscope itemtype="http://schema.org/WebPage">
@@ -31,7 +17,7 @@
 
         <!--HEADER-->
         <div itemscope itemtype="http://schema.org/WPHeader" class="row">
-            <img itemprop="description" src="img/markowski3.png" alt="markowski" title="markowski" class="markowski">
+            <img itemprop="description" src="img/logo_small.png" alt="albin vtc" title="albin vtc" class="albin">
         </div>
 
         <!-- <div class="row">
@@ -39,29 +25,26 @@
         </div> -->
 
         <!--MENTIONS LEGALES-->
-        <h1 itemprop="name">Mentions légales</h1>
-
-        <p>TEST</p>
-        
+        <h1 itemprop="name">Mentions légales</h1>        
         <div itemscope itemtype="http://schema.org/Thing" class="mentionslegales">
             <p itemprop="description"><span><b>Editeur du Site : </b></span><br>
-                SAS AREGE<br>
-                Numéro de SIRET :  75311915500012<br>
-                Responsable editorial : Monsieur Georges VITAL<br>
-                46 rue Paradis 75010 PARIS<br>
-                Téléphone :+ 33(0)1 48 00 83 07<br>
-                Email : contact@prince-jorge.com<br>
-                Site Web : <a href="http://http://www.markowski.fr">http://www.markowski.fr</a>
+                ALBIN VTC<br>
+                Numéro de SIRET : 50941104700038<br>
+                Responsable editorial : Monsieur Albin RODRIGUEZ<br>
+                <br>
+                Téléphone : 07.61.44.84.61<br>
+                Email : <br>
+                Site Web : <a href="https://www.albin-vtc.com">ALBIN VTC</a>
             </p>
             <p itemprop="description"><b><span>Hébergement :</span></b><br>
                 Hébergeur : OVH<br>
                 2 rue Kellermann – 59100 Roubaix<br>
-                Site Web :  <a href="http://https://www.ovh.com/fr/">https://www.ovh.com/fr/</a>
+                Site Web :  <a href="https://www.ovh.com/fr/">OVH</a>
             </p>
             <p itemprop="description"<span><b>Développement</b></span> : <br>
                 Aurélie BONET<br>
-                Adresse : 10 place Revoul 8460 VALREAS<br>
-                Site Web : <a href="http://http://aureliebonet.fr/">http://aureliebonet.fr/</a>
+                Adresse : 2 chemin de l'Oulle 8460 VALREAS<br>
+                Site Web : <a href="https://aureliebonet.fr/">http://aureliebonet.fr/</a>
             </p>
         </div>
     </div>
